245c3af60aca2327b66c8bbd79c5446373feb368
justinvdm <justinvdm.0.1.0@gmail.com>
2016-03-02 09:41:57 +0200

    Failing tests for titles

--- tests/numl.test.js
+++ tests/numl.test.js
@@ -2,7 +2,91 @@ var numl = require('../numl');
 
 
 describe("numl", function() {
-  it("should exist", function() {
-    numl.should.exist;
+  it("should parse dialogue titles", function() {
+    numl(`
+    # foo
+    `)
+    .should.shallowDeepEqual({
+       title: 'foo'
+    });
+
+    numl(`
+
+      #foo
+
+    `)
+    .should.shallowDeepEqual({
+       title: 'foo'
+    });
+  });
+
+  it("should parse sequence titles", function() {
+    numl(`
+    # foo
+    ## bar
+    ## baz
+    `)
+    .should.shallowDeepEqual({
+       title: 'foo',
+       sequences: [{
+         title: 'bar'
+       }, {
+         title: 'baz'
+       }]
+    });
+
+    numl(`
+
+      #foo
+
+      ##bar
+
+      ##baz
+
+    `)
+    .should.shallowDeepEqual({
+       sequences: [{
+         title: 'bar'
+       }, {
+         title: 'baz'
+       }]
+    });
+  });
+
+  it("should parse block titles", function() {
+    numl(`
+    # foo
+    ## bar
+    ### quux
+    ### corge
+    `)
+    .should.shallowDeepEqual({
+      sequences: [{
+        blocks: [{
+          title: 'quux'
+        }, {
+          title: 'corge'
+        }]
+      }]
+    });
+
+    numl(`
+      #foo
+
+      ##bar
+
+      ###quux
+
+      ###corge
+    `)
+    .should.shallowDeepEqual({
+      sequences: [{
+        blocks: [{
+          title: 'quux'
+        }, {
+          title: 'corge'
+        }]
+      }]
+    });
   });
 });
