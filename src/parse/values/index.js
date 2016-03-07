735c4dc9e480ddae397d31677805584e203999ce
justinvdm <justinvdm.0.1.0@gmail.com>
2016-03-07 11:56:32 +0200

    Fix bad whitespace

--- src/parse/values/index.js
+++ src/parse/values/index.js
@@ -2,17 +2,17 @@ var identity = require('lodash/identity');
 
 
 var types = {
-	symbol: identity
+  symbol: identity
 };
 
 
 function parse(value) {
-	var fn = types[value.type];
+  var fn = types[value.type];
 
-	if (!fn) throw new SyntaxError(
-		'SyntaxError: Unrecognised type "' + value.type + '".');
+  if (!fn) throw new SyntaxError(
+    'SyntaxError: Unrecognised type "' + value.type + '".');
 
-	return fn(value.value);
+  return fn(value.value);
 }
 
 
